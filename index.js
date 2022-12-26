36030276ce283fbf48c71595e0fb69971fa2babd
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2022-12-25 21:51:57 -0500

    configured middlewares

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send(`
+        <div>
+            <form method='POST'>
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="confirm password" />
+                <button>Sign up</button>
+            </form>
+        </div>
+    `);
 });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Account Created!');
+})
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
-})
+});
